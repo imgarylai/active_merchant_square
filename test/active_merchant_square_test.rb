2b062267dd929d2ab223bc19b668d8cb9fa0b2da
Gary Lai <garylai1990@gmail.com>
2020-06-07 00:46:01 +0800

    :construction_worker: (ci) remove unused test

--- test/active_merchant_square_test.rb
+++ test/active_merchant_square_test.rb
@@ -2,12 +2,8 @@
 
 require 'test_helper'
 
-class ActiveMerchantSquareTest < Minitest::Test
+class ActiveMerchantSquareTest < Test::Unit::TestCase
   def test_that_it_has_a_version_number
     refute_nil ::ActiveMerchantSquare::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
   end
 end
